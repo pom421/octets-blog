2443a2a0d310528a67fce47dffea4a416eb91b56
pom421 <mauguet.po@gmail.com>
2024-10-09 14:08:22 +0200

    feat: add tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,10 @@ import sitemap from '@astrojs/sitemap';
 
 import react from '@astrojs/react';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
     site: "https://octets-blog.vercel.app",
-    integrations: [mdx(), sitemap(), react()],
+    integrations: [mdx(), sitemap(), react(), tailwind()],
 });
